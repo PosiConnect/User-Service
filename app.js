6faf821c9fea7402696158a75343da1e42b1cb56
JClerve <joesonclerve9@gmail.com>
2025-02-23 23:32:24 +0530

    Updated server.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("../back-end/middlewares/error");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1", auth);
 
 module.exports = app;
